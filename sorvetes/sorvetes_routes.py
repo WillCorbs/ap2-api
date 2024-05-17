a57644778b2e3287c55ac18a3227b310095af916
Willian Emanuel Corban Silva <2301250@salas.aulas>
2024-05-16 21:38:14 -0300

    primeiro commit

---
+++ sorvetes/sorvetes_routes.py
@@ -0,0 +1,73 @@
+#from config import db
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for
+from .sorvetes_model import SorveteNaoEncontrado, listar_sorvetes, sorvete_por_id, adicionar_sorvete, atualizar_sorvete, excluir_sorvete
+
+sorvetes_blueprint = Blueprint('sorvetes', __name__)
+
+@sorvetes_blueprint.route('/', methods=['GET'])
+def getIndex():
+    return "Meu index"
+
+@sorvetes_blueprint.route('/sorvetes', methods=['GET'])
+def get_sorvetes():
+    sorvetes = listar_sorvetes()
+    return render_template("sorvetes.html", sorvetes=sorvetes)
+
+@sorvetes_blueprint.route('/sorvetes/<int:id_sorvete>', methods=['GET'])
+def get_sorvete(id_sorvete):
+    try:
+        sorvete = sorvete_por_id(id_sorvete)
+        return render_template('sorvete_id.html', sorvete=sorvete)
+    except SorveteNaoEncontrado:
+        return jsonify({'message': 'Sorvete não encontrado'}), 404
+  
+@sorvetes_blueprint.route('/sorvetes/adicionar', methods=['GET'])
+def adicionar_sorvete_page():
+    return render_template('criarSorvetes.html')
+
+@sorvetes_blueprint.route('/sorvetes', methods=['POST'])
+def create_sorvete():
+    sabor = request.form['sabor']
+    categoria = request.form['categoria']
+    preco = request.form['preco']
+    qtd_estoque = request.form['qtd_estoque']
+    novo_sorvete = {'sabor': sabor,
+                    'categoria': categoria,
+                    'preco': preco,
+                    'qtd_estoque': qtd_estoque}
+    adicionar_sorvete(novo_sorvete)
+    return redirect(url_for('sorvetes.get_sorvetes'))
+
+@sorvetes_blueprint.route('/sorvetes/<int:id_sorvete>/editar', methods=['GET'])
+def editar_sorvete_page(id_sorvete):
+    try:
+        sorvete = sorvete_por_id(id_sorvete)
+        return render_template('sorvete_update.html', sorvete=sorvete)
+    except SorveteNaoEncontrado:
+        return jsonify({'message': 'Sorvete não encontrado'}), 404
+
+@sorvetes_blueprint.route('/sorvetes/<int:id_sorvete>', methods=['PUT',"POST"])
+def update_sorvete(id_sorvete):
+    print("Dados recebidos no formulário:", request.form)
+    try:
+        sorvete = sorvete_por_id(id_sorvete)
+        sabor = request.form['sabor']
+        categoria = request.form['categoria']
+        preco = request.form['preco']
+        qtd_estoque = request.form['qtd_estoque']
+        sorvete['sabor'] = sabor
+        sorvete['categoria'] = categoria
+        sorvete['preco'] = preco
+        sorvete['qtd_estoque'] = qtd_estoque
+        atualizar_sorvete(id_sorvete, sorvete)
+        return redirect(url_for('sorvetes.get_sorvete', id_sorvete=id_sorvete))
+    except SorveteNaoEncontrado:
+        return jsonify({'message': 'Sorvete não encontrado'}), 404
+   
+@sorvetes_blueprint.route('/sorvetes/delete/<int:id_sorvete>', methods=['DELETE','POST'])
+def delete_sorvete(id_sorvete):
+    try:
+        excluir_sorvete(id_sorvete)
+        return redirect(url_for('sorvetes.get_sorvetes'))
+    except SorveteNaoEncontrado:
+        return jsonify({'message': 'Sorvete não encontrado'}), 404
